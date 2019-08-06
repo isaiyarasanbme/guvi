3f9e0146880718a56b8946dd16708aceb1322251
isaiyarasanbme <53809133+isaiyarasanbme@users.noreply.github.com>
2019-08-06 12:29:34 +0530

    Update pn.py

--- pn.py
+++ pn.py
@@ -1,7 +1,7 @@
 a=int(input())
 if a>0:
-      print('POSITIVE')
+      print('Positive')
 elif a<0:
-        print("NEGATIVE")
+        print("Negative")
 else :
-     print("ZERO")
+     print("Zero")
