c64a578621311d1dcecdba009b7898cff64473f4
isaiyarasanbme <53809133+isaiyarasanbme@users.noreply.github.com>
2019-08-06 12:58:38 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,6 +1,6 @@
 a=input()
 list1=['a','e','i','o','u']
-list2=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+list2=['c','d','b','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
 if a in list1:
              print("Vowel")
 elif a in list2:
